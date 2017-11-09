61c4af413317bdd41f9dafbfcf3f3f0126af6339
Alex Somesan <alex.somesan@gmail.com>
2017-11-09 15:58:21 +0100

    PowerDNS module: systemd unit doesn't use 'su' command

--- modules/aws-gov/powerdns/ignition.tf
+++ modules/aws-gov/powerdns/ignition.tf
@@ -1,5 +1,6 @@
 data "ignition_config" "powerdns" {
   count = "1"
+
   systemd = [
     "${data.ignition_systemd_unit.powerdns.id}",
   ]
@@ -32,11 +33,11 @@ data "ignition_file" "createzone" {
 sleep 30s
 docker exec pdns-master pdnsutil create-zone $1
 EOF
-}
+  }
 }
 
 data "ignition_systemd_unit" "powerdns" {
-  name    = "powerdns.service"
+  name   = "powerdns.service"
   enable = true
 
   content = <<EOF
@@ -51,16 +52,16 @@ RemainAfterExit=true
 User=root
 Group=root
 
-ExecStartPre=/bin/su -c 'docker pull quay.io/nicholas_lane/pdns:4.0-1'
-ExecStartPre=/bin/su -c 'docker pull mysql'
-ExecStartPre=/bin/su -c 'docker run -d --name=mysql -e MYSQL_ROOT_PASSWORD=changeme mysql'
-ExecStart=/usr/bin/bash /usr/bin/docker run --name pdns-master --link mysql:db -p 53:53/udp -p 53:53 -p 8081:8081 -d -e PDNS_ALLOW_AXFR_IPS=127.0.0.1 -e PDNS_DISTRIBUTOR_THREADS=3 -e PDNS_CACHE_TTL=20 -e PDNS_RECURSIVE_CACHE_TTL=10 -e DB_ENV_MYSQL_ROOT_PASSWORD=changeme -e PDNS_ZONE="${var.base_domain}" quay.io/nicholas_lane/pdns:4.0-1
+ExecStartPre=/usr/bin/docker pull quay.io/nicholas_lane/pdns:4.0-1
+ExecStartPre=/usr/bin/docker pull mysql
+ExecStartPre=/usr/bin/docker run -d --name=mysql -e MYSQL_ROOT_PASSWORD=changeme mysql
+ExecStart=/usr/bin/docker run --name pdns-master --link mysql:db -p 53:53/udp -p 53:53 -p 8081:8081 -d -e PDNS_ALLOW_AXFR_IPS=127.0.0.1 -e PDNS_DISTRIBUTOR_THREADS=3 -e PDNS_CACHE_TTL=20 -e PDNS_RECURSIVE_CACHE_TTL=10 -e DB_ENV_MYSQL_ROOT_PASSWORD=changeme -e PDNS_ZONE="${var.base_domain}" quay.io/nicholas_lane/pdns:4.0-1
 ExecStartPost=-/home/core/createzone "${var.base_domain}"
 
-ExecStopPost=/bin/su -c 'docker kill mysql'
-ExecStopPost=/bin/su -c 'docker rm mysql'
-ExecStopPost=/bin/su -c 'docker kill pdns-master'
-ExecStopPost=/bin/su -c 'docker rm pdns-master'
+ExecStopPost=/usr/bin/docker kill mysql
+ExecStopPost=/usr/bin/docker rm mysql
+ExecStopPost=/usr/bin/docker kill pdns-master
+ExecStopPost=/usr/bin/docker rm pdns-master
 
 [Install]
 WantedBy=multi-user.target
