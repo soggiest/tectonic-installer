1d8b05eddc188e6126ed10bb309eaaf3af9ca884
Alex Somesan <alex.somesan@gmail.com>
2017-11-09 14:52:28 +0100

    PowerDNS module: use optional Elastic IP

---
+++ modules/aws-gov/powerdns/eip.tf
@@ -0,0 +1,11 @@
+resource "aws_eip" "powerdns_endpoint" {
+  count = "${var.external_dns ? 1: 0}"
+  vpc   = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  count = "${var.external_dns ? 1: 0}"
+
+  instance_id   = "${aws_instance.powerdns_node.id}"
+  allocation_id = "${aws_eip.powerdns_endpoint.id}"
+}
