76370971f01a63f1f5832f44d6946bdfb57299d5
soggiest <nicholas.lane@coreos.com>
2017-12-15 15:34:53 -0800

    Reduced the size of the MySQL instance created for PowerDNS to t2.micro

--- modules/aws-gov/powerdns/server.tf
+++ modules/aws-gov/powerdns/server.tf
@@ -33,7 +33,7 @@ resource "aws_db_instance" "mysql" {
   storage_type           = "gp2"
   engine                 = "mysql"
   engine_version         = "5.7.17"
-  instance_class         = "db.t2.large"
+  instance_class         = "db.t2.micro"
   name                   = "db_create"
   username               = "root"
   password               = "powerdns"
